0e163c46d7117f399c553d52b67e7d04daa46b32
mbeche <mb.johnsonbece@gmail.com>
2019-06-02 11:34:28 -0500

    Implement responsive service in app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResponsiveService } from './responsive.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-awesome-app';
+
+  constructor(private responsiveService: ResponsiveService) {}
+
+  ngOnInit() {
+    this.responsiveService.getMobileStatus().subscribe( isMobile => {
+      if (isMobile) {
+        console.log('Mobile device detected')
+      } else {
+        console.log('Desktop detected')
+      }
+    });
+    this.onResize();
+  }
+
+  onResize() {
+    this.responsiveService.checkWidth();
+  }
 }
